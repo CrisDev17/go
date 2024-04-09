e826d838c77492480e418aadb684ff8c272aef78
unknown <trandien719@gmail.com>
2024-04-09 10:08:52 +0700

    update main.go

--- main.go
+++ main.go
@@ -13,5 +13,11 @@ func main() {
 		name := c.Param("name")
 		c.String(http.StatusOK, "Hello %s", name)
 	})
+	router.GET("/user/:name/*action", func(c *gin.Context) {
+		name := c.Param("name")
+		action := c.Param("action")
+		message := name + " is " + action
+		c.String(http.StatusOK, message)
+	})
 	router.Run(":3000")
 }
